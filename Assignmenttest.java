686768cef6257106f8c4b79cd571d17813f477d9
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-25 10:32:02 -0400

    Add files via upload
    assignment tested.

---
+++ Assignmenttest.java
@@ -0,0 +1,23 @@
+
+public class Assignmenttest {
+
+	public class ReturnStatements {
+
+		public static void main(String[] args) {
+			myMethod();
+			int sum =6+9;
+			System.out.println(sum);
+		}
+
+		public static void myMethod( ) {
+			System.out.println("This is my own method!");
+			
+		}
+		public static int add(int x, int y) {
+			return x+y;
+		    	
+		}
+		
+	}
+	
+}
