744c93aadaf973f3dc27072e3df142a589fb587b
Yadav <Yadhikari49@gmail.com>
2022-09-26 23:50:19 -0400

    Create variables.java
    homework.
    Variables

---
+++ src/variables.java
@@ -0,0 +1,48 @@
+
+public class variables {{
+int socialSecurityNumber;}
+{
+String productName = "Bike";
+String color = "blue";
+int quantity = 21;
+}
+
+{String lastName ;
+lastName = "Adhikari";
+}
+
+
+{double valueofpi;}
+
+
+
+{Long customerPhoneNumber = 8014562356l;}
+
+String customerCreditCard; 
+
+String CompnyContactinfo; 
+
+short height = 123; 
+
+float employeeSalary = 100500f;
+
+
+String cart1; 
+	int x;
+String cart2;
+	int y; 
+	
+return x+y;
+
+{
+	String paymentDetails;
+
+paymentDetails p = new paymentDetails(;)
+}
+
+
+
+
+
+
+}
