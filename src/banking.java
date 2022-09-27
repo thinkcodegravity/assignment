647b3ed0f724a379e0ba43d772d45b1f3d7c7fd8
Yadav <Yadhikari49@gmail.com>
2022-09-27 18:21:12 -0400

    Create banking.java
    banking

---
+++ src/banking.java
@@ -0,0 +1,45 @@
+
+public class banking {
+ address addr = new address();
+ 
+ employees emp = new employees();
+ 
+account acc =  new account();
+ 
+customers cust = new customers();
+
+public class address  {
+	
+	int houseNumber;
+	String streetName;
+	String city;
+	String state;
+	int zipcode;
+	
+}
+
+public class employees  {
+	int emplyeeID;
+	String firstName;
+	String lastName;
+	int dateBirth;
+	
+	
+	
+}
+
+public class account  {
+	
+	int accountNumber;
+	String customerfirstName;
+	String customerlastName;
+	
+}
+
+public class customers {
+	String customerfirstName;
+	String customerlastName;
+	
+}
+}
+
