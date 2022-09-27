a64d373cbbd9ae4e3d226ddcb8ccd38f600296e2
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-27 12:52:46 -0400

    Create Product.java
    Practice assignment

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+
+public class Product {
+
+	public static void main(String[] args) {
+		String fName = "Kamal";
+		Double Price = 3.25;
+		int Quantity = 12;
+		System.out.println(fName + " buy $" + Price + " of " + Quantity + " penciles.");
+
+	}
+
+}
