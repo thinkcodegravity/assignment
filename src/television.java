f23071e522cc05ae18a89eca8666f91589136aec
Yadav <Yadhikari49@gmail.com>
2022-09-26 23:51:18 -0400

    Create television.java
    tv

---
+++ src/television.java
@@ -0,0 +1,6 @@
+
+public class television {
+	
+	//television has:  screen, volume button, power button, network card, button to change channel, 
+//  does:  display image/video, change volume, turn on/off, connect to network.
+}
