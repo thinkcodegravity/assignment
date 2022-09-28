d3f6f11a8999f46635af3572ce004ede95b0a305
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-28 00:47:13 -0500

    Create product.java

---
+++ src/product.java
@@ -0,0 +1,18 @@
+
+	public class product {
+	String name="Iphone";
+	double price=999.9;
+	int quantity=10;
+
+	public double buyProduct() {
+		int productNumber=5;
+		double productCost=price*productNumber;
+		
+		System.out.println("Your total is $"+productCost+" for"+productNumber+" "+name);
+		return productCost; 
+
+	}
+
+	}
+
+
