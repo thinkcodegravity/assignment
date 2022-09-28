9ebfd7a74b691a5dace7dd0b0780864b2a4366d5
Yadav <Yadhikari49@gmail.com>
2022-09-27 21:17:14 -0400

    Create totalincart.java
    totalincart

---
+++ src/totalincart.java
@@ -0,0 +1,23 @@
+
+public class totalincart {
+	
+	totalincart tc = new totalincart();
+	
+	
+	public class tc {
+
+	int numberofitems;
+	int priceperitem ;
+	int totalprice ;
+	}
+
+
+public int mul(numberofitems , priceperitem ) {
+	
+	int mul = numberofitems * priceperitem; 
+	
+	return mul;
+	
+	// public int mul(int a, int b) {
+	// int mul=a*b;
+}
