de7cd3b558232a1499487ef41f835f0b4d95b603
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-27 21:17:10 -0400

    Tested assignment
    simple class create

--- src/Myclass.java
+++ src/Myclass.java
@@ -4,7 +4,11 @@ public class Myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String FName = "Kamal";
+		String Fname = "Kamal";
+		String Lname = "Saru";
+		
+		System.out.println("My name is "+Fname +Lname);
+		
 		
 	}
 
