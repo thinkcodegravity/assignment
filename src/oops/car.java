b94606397b339d9d80a0f8f510c6e683236c2dfe
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-28 13:40:39 -0500

    Create car.java

---
+++ src/oops/car.java
@@ -0,0 +1,25 @@
+package oops;
+
+public class car {
+	//Declaring variables
+	String name="Toyota Corolla";
+	int year=2020;
+	int door=4;
+
+public void driveCar( ) {
+	System.out.println("I Like to drive my car");
+}
+
+public void doorNumbers(int door) {
+	
+	System.out.println("My car has"+door+"door.");
+}
+
+
+	
+
+
+public static void main(String[] args) {
+	
+}
+}
