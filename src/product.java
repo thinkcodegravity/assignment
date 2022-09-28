7dfe101f44de3649a7a83d2fb1bd5bdfae04a4c7
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-28 00:52:09 -0500

    Update product.java

--- src/product.java
+++ src/product.java
@@ -1,18 +1,16 @@
 
-	public class product {
-	String name="Iphone";
-	double price=999.9;
-	int quantity=10;
+public class Product {
+String name="Iphone";
+double price=999.9;
+int quantity=10;
 
-	public double buyProduct() {
-		int productNumber=5;
-		double productCost=price*productNumber;
-		
-		System.out.println("Your total is $"+productCost+" for"+productNumber+" "+name);
-		return productCost; 
+public double buyProduct() {
+	int productNumber=5;
+	double productCost=price*productNumber;
+	
+	System.out.println("Your total is $"+productCost+" for"+productNumber+" "+name);
+	return productCost; 
 
-	}
+}
 
-	}
-
-
+}
