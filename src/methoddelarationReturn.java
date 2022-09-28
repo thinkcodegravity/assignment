85b7f80b7da00e895cb5850dde96726e993f8436
Yadav <Yadhikari49@gmail.com>
2022-09-27 21:18:24 -0400

    Create methoddelarationReturn.java

---
+++ src/methoddelarationReturn.java
@@ -0,0 +1,20 @@
+
+public class methoddelarationReturn {
+	registration reg = new registration();
+	
+	
+	
+	
+	public class registration {
+	
+		String firstName;
+		String lastName;
+		int dateofBirth;
+		String userID;
+		String password;
+		
+		
+		
+	}	
+	}
+ 
