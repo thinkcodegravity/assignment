3547bd5ee4188537e8807ab269dd42639cf696ea
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-28 13:40:10 -0400

    Television
    method for television.

--- src/Television.java
+++ src/Television.java
@@ -2,8 +2,19 @@
 public class Television {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String TVScreen = "Samsung LCD";
+		String SwitchTab ="ON/OFF";
+		String VolumeTab ="+/-";
+		String ChannelButton = "Up/Down";
+		
+		System.out.println("I have a " + TVScreen+".");
+		System.out.println("Press the button to turn "+ SwitchTab+".");
+		System.out.println("Press the " + VolumeTab + "button for increase/decrease the volume of the TV.");
+		System.out.println("Choose your favourate channel pressing the " + ChannelButton+".");
+		
+
+		
 	}
 
 }
