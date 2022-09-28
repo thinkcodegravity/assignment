2bdb9f7b1aea8d86be45c2342931c7e688eaedd6
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-28 17:02:30 -0500

    Create Television.java

---
+++ src/oops/Television.java
@@ -0,0 +1,31 @@
+package oops;
+
+public class Television {
+	public static void main(String[] args){
+	//Declaring variables
+	String televisionName="LG Smart 4K";
+	int size=55;
+	String connection="Satellite" ;
+	int channelCount=160;
+	
+	System.out.println("TV Name: "+televisionName);
+	System.out.println("Television Size: "+size+" inch");
+	System.out.println("Connection Type: "+connection);
+	System.out.println("Number of Channels: "+channelCount);
+	
+	}
+	//Tv Switch on Method
+	public void televisionOn() {
+		
+	}
+	//Tv Switch off Method
+	public void televisionOff() {
+	}
+	//Channel Change method
+	public void changeChannel() {
+		
+	}
+	}
+	
+
+
