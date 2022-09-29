4e2991937e92fbbd7f1a1869293d971d65904c53
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-29 16:21:03 -0500

    Calculator.java

---
+++ src/method/Calculator.java
@@ -0,0 +1,34 @@
+package method;
+
+public class Calculator {
+	
+	int a=5;
+	int b=10;
+	
+	
+	public int add(int Addision) {
+		Addision=a+b;
+		return Addision;
+	}
+	public int sub(int Substraction) {
+		Substraction=b-a;
+		return Substraction;
+	}
+	public int mul(int Multiplication) {
+		Multiplication=a*b;
+		
+		return Multiplication;
+	}
+	public int div(int Division) {
+		Division = b/a;
+		return Division;
+	}
+	public static void main() {
+		
+	}
+	
+
+	
+
+}
+
