84ea3044a73b1fba893eba664a155bad7d483c9c
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-29 14:32:32 -0500

    product.java

--- src/oops/product.java
+++ src/oops/product.java
@@ -2,11 +2,15 @@ package oops;
 
 
 public class product {
-
+	public static void main(String[] args) {
+		
 String name="Samsung";
 double price=999.9;
 int quantity=10;
+double totalPrice=quantity*price;
 
+	System.out.println("The Price of "+quantity+" "+name+" is: $"+totalPrice);
+}
 
 public String productBuy(String samsungGalaxyS22){
 	return samsungGalaxyS22;}
@@ -14,14 +18,12 @@ public String productBuy(String samsungGalaxyS22){
 
 public double productSell(double totalPrice, double price, int quantity) {
 	totalPrice=price*quantity;
-	{
-		System.out.println("The Price of "+quantity+" "+name+"is: $"+totalPrice);
-	}
+	
 return totalPrice;
 }
-public static void main(String[] args) {
+
 }
-}
+
 
 
 
