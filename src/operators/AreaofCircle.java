d776a243d351ce21ef64f3f69cb4605331e66489
Yadav <Yadhikari49@gmail.com>
2022-09-28 22:47:42 -0400

    Create AreaofCircle.java
    circle

---
+++ src/operators/AreaofCircle.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class AreaofCircle {
+
+	int radius;
+	double π = 3.14;
+
+	
+	// area of circle = π*r*r
+	
+	  double AreaofCircle(int radius, ) {
+		
+		int R = radius*radius;
+		double AreaofCircle = R*π;
+	  
+	
+		return AreaofCircle;
+	}
+
