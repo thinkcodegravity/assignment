3c974002cbe7d6ce6df836e34d4f828f0af35e0d
Yadav <Yadhikari49@gmail.com>
2022-09-28 20:17:45 -0400

    Create bankProgram.java
    operators

---
+++ src/operators/bankProgram.java
@@ -0,0 +1,55 @@
+package operators;
+
+public class bankProgram {
+	
+	
+int  balance;
+
+public int withdraw(int withdrawAmount)
+{
+if (balance>=withdrawAmount)	{
+	System.out.println("amount withdraw successful");
+	
+} else {
+	System.out.println("Insufficient Fund");
+}
+	
+return  withdrawAmount;
+}
+
+
+
+	public int newBalance( int balancewithdeposit ) {
+	
+balancewithdeposit NB = new balancewithdeposit() {
+		{
+		
+		System.out.println("new Balance");
+		
+		return newBalnce;
+		
+	}
+	
+		public int NB {
+			
+		int balance;
+			int deposit;
+			int withdraw;
+			
+			NB = (balance + deposit - withdraw);
+		}
+			
+		};
+	
+
+
+
+
+
+
+
+
+
+		
+	}
+
