9a6052c538f50bfb229cb844e42ef0183f8fd1db
Yadav <Yadhikari49@gmail.com>
2022-09-28 20:17:51 -0400

    Create balancewithdeposit.java

---
+++ src/operators/balancewithdeposit.java
@@ -0,0 +1,24 @@
+package operators;
+
+public class balancewithdeposit {
+		
+		balancewithdeposit NB = new balancewithdeposit() {
+				{
+				
+				System.out.println("new Balance");
+				
+				return newBalance;
+				
+			}
+			
+				public class NB {
+					
+					int balance;
+					int deposit;
+					int withdraw;
+					
+					NB = balance + deposit - withdraw }
+				
+				}
+				}
+}
