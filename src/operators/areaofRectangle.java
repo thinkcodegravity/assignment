6f1c5b6ecd318da155a215b2a0f52cba058dc167
Yadav <Yadhikari49@gmail.com>
2022-09-28 22:47:25 -0400

    Create areaofRectangle.java
    rectangle

---
+++ src/operators/areaofRectangle.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class areaofRectangle {
+
+	int  length = 5;
+	int breadth = 3;
+	
+	public int area(int length, int breadth) {
+		
+		int area=length*breadth;
+		
+		return area;
+		
+	}
+
+		
+		
+}
