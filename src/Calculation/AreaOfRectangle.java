0ed9b9e0ee8add5640c27b27cc2a54130c8c5ebd
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-30 11:28:53 -0400

    Area of Rectangle with user inpu

--- src/Calculation/AreaOfRectangle.java
+++ src/Calculation/AreaOfRectangle.java
@@ -6,6 +6,8 @@ class AreaOfRectangle {
 
 	public static void main(String[] args) {
 		//Calculation Area of the Rectangle
+		//Full scanner version or input user
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the legth of Rectangle : - ");
 		double lenth = scanner.nextDouble();
