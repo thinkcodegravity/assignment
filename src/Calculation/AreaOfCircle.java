269c68cd9a3911294d199947d5a2554ce33008b3
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-09-30 11:25:08 -0400

    Area of circle with setup two decimal format

--- src/Calculation/AreaOfCircle.java
+++ src/Calculation/AreaOfCircle.java
@@ -1,9 +1,24 @@
 package Calculation;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class AreaOfCircle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Calculation of the Area of Circle
+		//Area of the cirlce is a=r*r*22/7
+		//Workin the full scanner and decimal format
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the radius of the circle :" );
+		double r = scanner.nextDouble();
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
+		
+		double a = r*r*Math.PI;
+		System.out.println("The are of the circle is  " +(df.format(a)));
+		
 
 	}
 
