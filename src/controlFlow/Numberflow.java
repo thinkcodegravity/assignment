8240f1793b8111f2c81d718219ffce1957a2245f
Yadav <Yadhikari49@gmail.com>
2022-09-30 19:59:31 -0400

    Create Numberflow.java
    Number flow

---
+++ src/controlFlow/Numberflow.java
@@ -0,0 +1,24 @@
+package controlFlow;
+
+public class Numberflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter 1 for deposit, 2 for withdraw. 3 for balance check");
+		Scanner sc = new Scanner(System.in);
+		int choice= sc.nextInt(); 
+		if(choice ==1);
+		System.out.println("Enter deposit amount");
+	}
+	else if(choice == 2)		
+		System.out.println("Enter withdraw Amount");
+}
+else if (choise==3)
+		System.out.println(" Your balance $125");
+	}
+
+	
+
+}
+
