5f8d2cd28f7585c918b8e3eb4d585e9b759f8ecb
Yadav <Yadhikari49@gmail.com>
2022-09-30 19:59:49 -0400

    Create ExistingUserID.java
    userID

---
+++ src/operators/ExistingUserID.java
@@ -0,0 +1,18 @@
+package operators;
+
+
+	/*
+	 * Create website program
+	(has-a)Declare global variable existingUserid 
+	Create register method that takes new registeration userid and password
+	Based on If exsting userid matches new registeration id then registration fails else registeration method print success or failure.
+	 */
+public class ExistingUserID {
+	
+public void userID( string S) {
+	newuserid = S;
+}
+
+public static void main(String [] args) {
+	ExistingUserID ID = new ExistingUserID();
+	
