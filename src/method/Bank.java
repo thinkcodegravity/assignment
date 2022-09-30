d245a7238d678b6b0745a272e530f9f13fcf726c
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-09-29 22:28:20 -0500

    Create Bank.java

---
+++ src/method/Bank.java
@@ -0,0 +1,26 @@
+package method;
+
+public class Bank {
+	int balance=5000;
+	int depositAmount=100;
+	int withdrawAmount=500;
+	
+	public void withdraw() {
+		 
+		int newBalance=balance-withdrawAmount;
+		System.out.println("Your new balance after withdraw is $"+newBalance);
+	}
+	public void deposit() {
+		 
+		int newBalance=balance+depositAmount;
+		System.out.println("Your new balance after deposit is $"+newBalance);
+	}
+
+	public static void main(String[] args) {
+		
+ Bank newBalance=new Bank();
+ newBalance.withdraw();
+ newBalance.deposit();
+ }
+
+}
