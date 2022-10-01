ce1478f384fd811533f7fca36c09045f9ed0fad7
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-01 11:02:58 -0400

    Calculate area of circle with user input

---
+++ src/CalculatorPrograms/AreaOfCircle.java
@@ -0,0 +1,25 @@
+package CalculatorPrograms;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		//Calculation of the Area of Circle
+		//Area of the cirlce is a=r*r*22/7
+		//Workin the full scanner and decimal format
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the radius of the circle :" );
+		double r = scanner.nextDouble();
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(2);
+		
+		double a = r*r*Math.PI;
+		System.out.println("The area of the circle is  " +(df.format(a)));
+		
+
+	}
+
+}
