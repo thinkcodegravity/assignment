1d8b8827aa858915bd929e04ec61eb91115bee74
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-01 12:10:47 -0400

    Update AreaOfTriangle.java

--- src/CalculatorPrograms/AreaOfTriangle.java
+++ src/CalculatorPrograms/AreaOfTriangle.java
@@ -3,7 +3,7 @@ package CalculatorPrograms;
 public class AreaOfTriangle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Area of Triangle with user input
 
 	}
 
