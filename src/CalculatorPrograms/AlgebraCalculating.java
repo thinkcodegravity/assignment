acf8e773771394e3c6df9657106271e68851166b
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-01 14:21:17 -0400

    Calculation of algebra with the Power(Mathematic Power code)

--- src/CalculatorPrograms/AlgebraCalculating.java
+++ src/CalculatorPrograms/AlgebraCalculating.java
@@ -1,10 +1,26 @@
 package CalculatorPrograms;
 
+import java.util.Scanner;
+
 public class AlgebraCalculating {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the value of a :  ");
+		float a = scanner.nextFloat();
+		
+		System.out.println("Enter the value of b : ");
+		float b = scanner.nextFloat(); 
+		
+		//This code for the Math Power
+		//formula (a+b)2 =a2 +2ab+b2
+		double c = Math.pow(a,2) + 2*a*b + Math.pow(b, 2);  
+		
+		System.out.println("The value of (a + b)2 is " + c);
+		
+		
 	}
 
 }
