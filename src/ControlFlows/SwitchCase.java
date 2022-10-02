6c7a4a7e76a5767a3deb71d0a48c56edb8effb80
DevidKarki <108104786+Devid23@users.noreply.github.com>
2022-10-02 12:32:40 -0500

    switch case

--- src/ControlFlows/SwitchCase.java
+++ src/ControlFlows/SwitchCase.java
@@ -1,5 +1,33 @@
 package ControlFlows;
 
-public class SwitchCase {
+import java.util.Scanner;
 
+public class SwitchCase {
+	// main method
+	public static void main(String[] args) {
+		boolean value = true;
+		while (true) {
+			// accepts user input
+			Scanner sc = new Scanner(System.in);
+
+			// prompts user to enter a number
+			System.out.println();
+			System.out.println("Please enter a number: ");
+			int num = sc.nextInt();
+
+			switch (num) {
+			case 1:
+				System.out.println("The price for Large pizza is $17.99");
+				break;
+
+			case 2:
+				System.out.println("The price of pasta is $7.99");
+				break;
+
+			default:
+				System.out.println("The price of salad is $9.99");
+
+			}
+		}
+	}
 }
