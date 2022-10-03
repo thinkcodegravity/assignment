bedb777a817fef3199067eb2727b2548bf386728
Yadav <Yadhikari49@gmail.com>
2022-10-03 13:06:11 -0400

    Create Calculator.java
    Calculator

---
+++ src/operators/Calculator.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class Calculator {
+	public int add(int x, int y) {
+		return x+y;
+
+	}
+		
+	public int sub(int a, int b) {
+		return a-b;
+	}
+
+	public int div(int a1, int b1) {
+		return a1-b1;
+		
+	}
+	public int mul(int s, int t) {
+		return s*t;
+		
+	}
+	}
+
