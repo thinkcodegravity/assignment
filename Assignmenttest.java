b70644bec0ee7e937438d3ea0e2baad1bee53c0d
andy aladdin <as.bestinclass@gmail.com>
2022-10-03 19:06:40 -0500

    First Commit test

--- Assignmenttest.java
+++ Assignmenttest.java
@@ -4,19 +4,8 @@ public class Assignmenttest {
 	public class ReturnStatements {
 
 		public static void main(String[] args) {
-			myMethod();
-			int sum =6+9;
-			System.out.println(sum);
-		}
 
-		public static void myMethod( ) {
-			System.out.println("This is my own method!");
-			
-		}
-		public static int add(int x, int y) {
-			return x+y;
-		    	
-		}
+			System.out.println("Roshan Branch Test");
 		
 	}
 	
