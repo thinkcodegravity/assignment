23aece47d1f5b7841cbf12df88e8bcb52d7b5e28
ashre014 <113379381+ashre014@users.noreply.github.com>
2022-10-06 04:58:48 +0545

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,48 @@
+public class Variable {
+		
+		// to store Social security number.  Just do the declaration.
+		
+		int ssn;
+		
+		// to store Product Details. Declare and initialize on same line
+		
+		String productName = "soda";
+		int price = 15;
+		float vol = 0.75f;
+		
+		// to store last name.. Declare and next line initialize.
+		
+		//String lastName;
+		//lastName = "Shre";
+		
+		// to Store customer online status. Just Declare
+		
+		boolean onlineStatus;
+		
+		// To store value of pi . Just declare
+
+		double Pi;
+		
+		//to store customer phone number. Declare and initialize on same line
+		
+		long phoneNum = 677788488;
+		
+		// to store add 2 cart details. Just Declare
+		
+		String productName1;
+		byte size;
+		String color;
+		byte quantity;
+		
+		// to store shipping address Declare and initialize on same line
+		Addr ShippingAddress;
+		public class Addr
+		{
+			short houseNumber = 4000;
+			String streetName = "kingsway";
+			int street1 = 52;
+			short zip = 30112;
+			
+		}
+}
+
