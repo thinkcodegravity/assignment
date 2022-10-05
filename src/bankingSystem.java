8ae4ef423a765ca6417b6ffb08cc59be10923f18
ashre014 <113379381+ashre014@users.noreply.github.com>
2022-10-06 04:45:18 +0545

    bankingSystem

---
+++ src/bankingSystem.java
@@ -0,0 +1,21 @@
+public class bankingSystem {
+
+	name customerName;
+	long phoneNumber;
+	long accNumber;
+	add address;
+	
+			public class name {
+			
+			String fname;
+			String mname;
+			String lname;
+		
+	}
+			public class add {
+				short houseNumber;
+				String streetName;
+				int streetNumber;
+				int zip;
+			}
+}
