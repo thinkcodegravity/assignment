ddd33f57e143c2c3d46b478656711efd430c3f0f
Yadav <Yadhikari49@gmail.com>
2022-10-05 20:31:34 -0400

    Create PrintMenu.java
    PrintMenu

---
+++ src/controlFlow/PrintMenu.java
@@ -0,0 +1,30 @@
+package controlFlow;
+
+import java.util.Scanner;
+
+public class PrintMenu {
+		/*
+		 * Write a method to print menu items (use Switch case)
+		Display the price for item selected
+		1 – Pizza
+		2 – Pasta
+		Default – salad
+		 */
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		String choice = "Salad";
+		switch  (choice) {
+		case "1":
+			System.out.println("Pizza");
+			break;
+		case "2":
+			System.out.println("Pasta");
+			break;
+			default:
+				System.out.println("Salad");
+		}
+	}
+
