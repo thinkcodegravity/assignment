f3b2ff6cc895007cba478e903f155edd2e50d5e4
Yadav <Yadhikari49@gmail.com>
2022-10-06 19:54:13 -0400

    Update ExistingUserID.java

--- src/operators/ExistingUserID.java
+++ src/operators/ExistingUserID.java
@@ -8,22 +8,30 @@ package operators;
 	Based on If exsting userid matches new registeration id then registration fails else registeration method print success or failure.
 	 */
 public class ExistingUserID {
-	
+Long phonereg = 4789651230l;	
 String userID ="yadav253";
 String Password = "java963";
 
-public ExistingUserID( String U) {
+public String ExistingUsrID( long phoneInput) {
 	
-System.out.println(U);
-if (ExistingUserID == userID)
-{
-	
-System.out.println(user Id retrieved);
+if (phoneInput== phonereg) {
+	System.out.println(userID);
 }
-
 else{
-	System.out.println(Invalid user ID);
+	System.out.println("can't find user ID");
+}
+ return userID;
 }
 
 
+public static void main(String[] args ) {
+	ExistingUserID UID = new ExistingUserID();
+	
+	String userID = UID.ExistingUsrID(4789651230);
+	System.out.println("userid retrieved is:"  +userID );
+	
+	
+}
+	
+	
 }
