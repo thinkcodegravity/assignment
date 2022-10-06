ca230d5cb89dccd37d4068f1dfe8d26737cb932d
Yadav <Yadhikari49@gmail.com>
2022-10-05 21:10:56 -0400

    Create InputusingLoop.java
    input loop

---
+++ src/controlFlow/InputusingLoop.java
@@ -0,0 +1,32 @@
+package controlFlow;
+
+public class InputusingLoop {
+
+	/*Write a method that takes number as input using while loop
+		Print multiple of 1 to 10 for this input
+			 * 
+			 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i =1;
+		while( i<10 ){
+
+			i++;
+continue;
+
+		}
+		
+		
+	{
+		for( int i=1 ; i<=10 ; i++){
+	}
+			
+			if(i==15){
+				break;
+			}
+			System.out.println("1");
+		}
+
+	}
+
+}
