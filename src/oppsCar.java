ea81e0d9b863014d4ed2e49a33831f8a1d616d18
ashre014 <113379381+ashre014@users.noreply.github.com>
2022-10-07 19:19:51 +0545

    Update oppsCar.java

--- src/oppsCar.java
+++ src/oppsCar.java
@@ -1,23 +1,15 @@
 public class oppsCar {
 
-	//public static void main(String[] args)
-	//{
+	public static void main(String[] args){
 	
 	boolean acceleration = true;
 	boolean brake = false;
 		
-	public void moving (String moving)
-	{
-		while ( acceleration == true ) 
-			{
+		while ( acceleration == true ) {
 				System.out.println("car is moving");
 			}
-	}
-			
-	public void stop (String stop)
-	{
-		while ( brake == true ) 
-			{
+	
+	while ( brake == true ) {
 				System.out.println("car is not moving");
 			}
 	}
