b33f3aaac7b5fba98ad0e476af50f011f2e752bb
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-09 09:32:16 -0400

    Calculation of Interest Rate(%) of the loan.

--- src/ControlFlows/interestRate.java
+++ src/ControlFlows/interestRate.java
@@ -1,9 +1,28 @@
 package ControlFlows;
 
+import java.util.Scanner;
+
 public class interestRate {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Calculation of Interest Rate, 
+		//Simple interest SI= PTR/100
+		// formula for Interest Rate, R= (I*100)/(P*T)
+		//PrincipleAmount(P),Interest Amount(I), Time(T) and Interest Rate(R)
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the principle amount :-$ ");
+		double P = sc.nextDouble();
+		System.out.println("Enter the Simple Interest Amount :-$ ");
+		double I = sc.nextDouble();
+		System.out.println("Enter the duration of the loan :- ");
+		double T = sc.nextDouble();
+		
+		//Interest Rate formula
+		double R = I*100/P*T;
+		System.out.println("The interest Rate of the loan is " + R+"% ");
+		
 
 	}
 
