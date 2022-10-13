7414f1c1e782fcdac71c0876c6d6b0e4d23ed5f9
Yadav <Yadhikari49@gmail.com>
2022-10-13 01:56:26 -0400

    Create Calculator.java
    Inheritance

---
+++ src/inheritance/Calculator.java
@@ -0,0 +1,29 @@
+package inheritance;
+
+/*
+ * Write a program for calcualator
+Method add that takes 2 int input and returns int
+Method sub that takes 2 int input and returns int
+Method mul that takes 2 int input and returns int
+Method div that takes 2 int input and returns int
+ */
+public class Calculator {
+
+	public int add(int x, int y) {
+		return x + y;
+
+	}
+
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
+	public int mul(int x, int y) {
+		return x * y;
+
+	}
+
+	public int div(int x, int y) {
+		return x / y;
+	}
+}
