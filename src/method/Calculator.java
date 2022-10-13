6c3a0e89d51c30e5d1c08fb7aa3d708f8397788d
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-13 17:30:08 -0500

    Update Calculator.java

--- src/method/Calculator.java
+++ src/method/Calculator.java
@@ -1,33 +1,33 @@
 package method;
 
 public class Calculator {
-	int a=5;
-	int b=10;
-	public void add () {
+	int a;
+	int b;
+	public void add (int a, int b) {
 	int	addision=a+b;
 		System.out.println("Addition: " +addision);
 	}
-	public void sub() {
+	public void sub(int b,int a) {
 		int substraction=b-a;
 		System.out.println("Substraction: " +substraction);
 		
 	}
-	public void mul() {
+	public void mul(int a, int b) {
 		int multiplication=a*b;
 		
 		System.out.println("Multiplication: "+multiplication);
 	}
-	public void div() {
+	public void div(int b, int a) {
 		int division = b/a;
 		System.out.println("Division: "+division);
 	}
 
 	public static void main(String[] args) {
 		Calculator ans = new Calculator();
-		ans.add();
-		ans.sub();
-		ans.mul();
-		ans.div();
+		ans.add(2,5);
+		ans.sub(5,3);
+		ans.mul(2,3);
+		ans.div(6,3);
 		
 				
 
