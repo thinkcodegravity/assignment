1a9c66260ce1bc0965b5a9170e746b4121337096
Yadav <Yadhikari49@gmail.com>
2022-10-14 18:29:23 -0400

    Create Tester.java

---
+++ src/inheritance/Tester.java
@@ -0,0 +1,22 @@
+package inheritance;
+/*
+ * Tester
+Has name, age, salary, jobTitle
+Does talk, walk, work ( just sysout in this method is fine
+ */
+public class Tester extends Employee {
+
+	String jobTitle = "Manager";
+	
+	public  Tester() {
+		
+		
+			System.out.println("A Tester talks, walks, and works");
+			
+			
+			
+		}
+		
+		
+		
+	}
