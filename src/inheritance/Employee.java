ceff6f487acf0a31b56f132dc5ec9a19575d7638
Yadav <Yadhikari49@gmail.com>
2022-10-14 18:29:34 -0400

    Create Employee.java

---
+++ src/inheritance/Employee.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class Employee extends person {
+	
+	double salary= 50000;
+	
+public static void main (String [] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Employee talks");
+	System.out.println("Employee walks");
+
+}
+}
