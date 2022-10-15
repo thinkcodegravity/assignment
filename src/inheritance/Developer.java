e2c5e0f630213126ba4c9f96d785a082e74d12d7
Yadav <Yadhikari49@gmail.com>
2022-10-15 19:52:41 -0400

    Update Developer.java

--- src/inheritance/Developer.java
+++ src/inheritance/Developer.java
@@ -1,14 +1,14 @@
 package inheritance;
 
 public class Developer extends Tester {
-	
-	public static void main(String [] args){
 
-		Tester T= new Tester();
+	public static void main(String[] args) {
+		Tester T = new Tester();
 		
-	System.out.println("person name: " +T.name);	
-	System.out.println("Salary: " +T.salary);
-	
-	System.out.println("Title : " +T.jobTitle);
-}
+		System.out.println("Developer name: " + T.name);
+		System.out.println("Developer age; "+ T.age);
+		System.out.println("Developer works");
+		System.out.println("Title : " + T.jobTitle);
+		System.out.println("Salary: " + T.salary);
+	}
 }
