236846e6143ea69c7eb068e1e3a91cc4b17aa4a8
Yadav <Yadhikari49@gmail.com>
2022-10-15 19:52:34 -0400

    Update Tester.java

--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -1,4 +1,5 @@
 package inheritance;
+
 /*
  * Tester
 Has name, age, salary, jobTitle
@@ -6,17 +7,17 @@ Does talk, walk, work ( just sysout in this method is fine
  */
 public class Tester extends Employee {
 
-	String jobTitle = "Manager";
-	
-	public  Tester() {
+	 String jobTitle = "Manager";
+
+	public static void main(String[] args) {
 		
 		
-			System.out.println("A Tester talks, walks, and works");
-			
-			
-			
-		}
-		
-		
-		
+		Employee E = new Employee();
+		System.out.println("Nama: " +E.name);
+		System.out.println("Age: " +E.age);
+		System.out.println("Salary " +E.salary);
+		System.out.println("Job Title: " + jobTitle);
+		System.out.println("A Tester talks, walks, and works");
+
 	}
+}
