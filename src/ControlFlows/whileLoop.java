57ad70867bb9eb2a66544d5517729b472c5b48e1
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-16 13:10:48 -0400

    Multiplication table test using while loop

--- src/ControlFlows/whileLoop.java
+++ src/ControlFlows/whileLoop.java
@@ -3,8 +3,18 @@ package ControlFlows;
 public class whileLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Multiply of 6 between 1 to 10
+		//Using the while loop
+		//Multiply table for Multiplications
+		System.out.println("Multiply of 6 between 1 to 10 are;");
+		int number=6;
+		int i =1;
+		while (i<=10) {
+			System.out.println(number+"*"+i+"="+number*i);
+			i++;
+		}
+		
 	}
-
+	
 }
+		
