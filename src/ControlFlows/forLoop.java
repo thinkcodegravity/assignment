604db4a5a025f08f8233ee1af61a9219ce567e17
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-16 12:01:10 -0400

    Multiple of 5 between the 1 to 10 using for loop

--- src/ControlFlows/forLoop.java
+++ src/ControlFlows/forLoop.java
@@ -3,8 +3,14 @@ package ControlFlows;
 public class forLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		//Print the multiple of 5 between the 1-10
+		//Multiple of 5 is 5*1=5, 5*2=10
+		//Multiple of 5 between 1-10 is 5 and 10
+		for (int number =1; number<=10; number++) {
+			//check if number is multiple of 5
+			if(number%5==0 ) {
+				System.out.println(number); //multiple of 5 are 5 and 10
+			}
+		}
+	}	
 }
