6b01d6e51066959c1c06e179ad95fd2cf66c3c31
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-19 16:15:11 -0500

    Create PrintOneToTen.java

---
+++ src/Assignment2/PrintOneToTen.java
@@ -0,0 +1,20 @@
+package Assignment2;
+
+public class PrintOneToTen {
+	public void  print() {
+		for(int i=1; i<=10;i++) {
+			System.out.print(" "+i+" ");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		
+		PrintOneToTen p= new PrintOneToTen();
+		p.print();
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
