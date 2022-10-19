57f2a2e080ca3f7684239fa63acf8da8d9ccc30e
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-19 17:13:01 -0500

    Create MultipleOfFive.java

---
+++ src/Assignment2/MultipleOfFive.java
@@ -0,0 +1,16 @@
+package Assignment2;
+
+public class MultipleOfFive {
+
+	public static void main(String[] args) {
+		System.out.println("The muliple of 5:");
+		for(int i=1; i<=10; i++)
+		{
+			
+			System.out.println("5 * "+i+" = "+(5*i));
+		}
+		
+
+	}
+
+}
