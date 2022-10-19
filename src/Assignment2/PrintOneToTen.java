a29570e604af8959473da156e0bacd0c92326681
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-19 17:00:36 -0500

    Update PrintOneToTen.java

--- src/Assignment2/PrintOneToTen.java
+++ src/Assignment2/PrintOneToTen.java
@@ -1,17 +1,19 @@
 package Assignment2;
 
 public class PrintOneToTen {
+	
+	//Creating method print
 	public void  print() {
 		for(int i=1; i<=10;i++) {
 			System.out.print(" "+i+" ");
 		}
 		
 	}
-
+	//main method
 	public static void main(String[] args) {
-		
+		//Creating an object p
 		PrintOneToTen p= new PrintOneToTen();
-		p.print();
+		p.print();//Method calling statement
 		// TODO Auto-generated method stub
 		
 
