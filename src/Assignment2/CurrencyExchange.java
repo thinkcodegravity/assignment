9c638fc2e4560a8b603cadd962adefa498926fc5
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-19 16:00:09 -0500

    Update CurrencyExchange.java

--- src/Assignment2/CurrencyExchange.java
+++ src/Assignment2/CurrencyExchange.java
@@ -1,22 +1,29 @@
 /*
- * This program is converting the value of dollars into the rupee
+ * This program is converting the value of dollars into the Rs.
  */
 package Assignment2;
 import java.util.Scanner;
 public class CurrencyExchange {
+	int dollar;
+	double exchangeRupee;
+	
+	public void exchangeValue(int dollar) {
+		//Calculating the exchange value
+		 exchangeRupee=dollar*109.5;
 
+		System.out.println("The exchange amount of $"+dollar+ " to rupee is Rs "+exchangeRupee);
+		}
 	public static void main(String[] args) {
-		int dollar;
-		double exchangeRupee;
+		
 		//Creating object Scanner to read the input from the user
 		Scanner console=new Scanner(System.in);
 		
-		System.out.println("Please enter your dollor amount: ");
-		dollar=console.nextInt();
-		//Calculating the exchange value
-		exchangeRupee=dollar*109.5;
+		System.out.print("Please enter your dollor amount: $");
+		int dollar=console.nextInt();
+		CurrencyExchange exchange=new CurrencyExchange();
+		exchange.exchangeValue(dollar);
 		
-		System.out.println("The exchange amount of $"+dollar+ " to rupee is Rs "+exchangeRupee);
+	
 		}
 
 }
