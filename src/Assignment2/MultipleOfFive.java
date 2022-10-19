deca3816e9cb5da847eeb9c36bec5c2fff3118c7
Sandesh244 <sandeshkhatiwada24@gmail.com>
2022-10-19 17:34:37 -0500

    Update MultipleOfFive.java

--- src/Assignment2/MultipleOfFive.java
+++ src/Assignment2/MultipleOfFive.java
@@ -1,9 +1,15 @@
+/*
+ * This program is created to give the multiple of 5 from 1 to 10
+ */
 package Assignment2;
 
 public class MultipleOfFive {
 
+	//Main Method
 	public static void main(String[] args) {
 		System.out.println("The muliple of 5:");
+		
+		//Using for loop to calculate the multiple of 5 using increment
 		for(int i=1; i<=10; i++)
 		{
 			
