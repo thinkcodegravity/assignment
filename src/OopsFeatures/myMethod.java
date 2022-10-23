ae8cd5924e16752e2957e7f8a7dd42fff08d35b6
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-23 11:45:23 -0400

    call methods

---
+++ src/OopsFeatures/myMethod.java
@@ -0,0 +1,26 @@
+package OopsFeatures;
+
+public class myMethod {
+
+	//******************************Call Method**********************************
+	// Create a fullThrottle() method
+	public void fullThrottle() {
+	    System.out.println("The car is going as fast as it can!");
+		  }
+
+		  // Create a speed() method and add a parameter
+		  public void speed(int maxSpeed) {
+		    System.out.println("Max speed is: " + maxSpeed);
+		  }
+
+	// Inside Method, call the methods on the myCar object
+     public static void main(String[] args) {
+		    myMethod myCar = new myMethod();   // Create a myCar object
+		    myCar.fullThrottle();      // Call the fullThrottle() method
+		    myCar.speed(200);          // Call the speed() method
+		  }
+		}
+
+
+// The car is going as fast as it can!
+// Max speed is: 200
