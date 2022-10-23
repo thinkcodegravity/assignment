9b6c7fa41423ba95f975792fb64c7526d8b44275
Yadav <Yadhikari49@gmail.com>
2022-10-22 22:36:55 -0400

    Update Cart.java

--- src/inheritance/Cart.java
+++ src/inheritance/Cart.java
@@ -5,15 +5,10 @@ package inheritance;
 	 */
 public class Cart extends Calculator{
 	
-	public static void main ( String [] args) {
+	
 int productPrice=34;
 int quantity= 16;
 
-Calculator Cr = new Calculator();
 
-	int TotalValue = Cr.mul(productPrice, quantity);
-	
-	System.out.println("Total Price: $"  +TotalValue);
-	}
 }
 	
