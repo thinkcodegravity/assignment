b06511e84091810986849fea1d8a729863016402
Kamal saru <108743661+KamalSaru@users.noreply.github.com>
2022-10-23 11:45:02 -0400

    object call

---
+++ src/OopsFeatures/Object.java
@@ -0,0 +1,16 @@
+package OopsFeatures;
+
+//*************************************Java Class/Objects*******************************
+public class Object {
+	int x = 6;
+	
+
+	public static void main(String[] args) {
+		Object myObj1 = new Object();  //Create object instead of Main(Object)
+		Object myObj2 = new Object();  //object
+		
+		System.out.println(myObj1.x);
+		System.out.println(myObj2.x);
+	}
+
+}
