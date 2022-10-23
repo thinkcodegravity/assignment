f28534ea717dc74a257f909c636bbf69a2efd36d
Yadav <Yadhikari49@gmail.com>
2022-10-22 22:36:46 -0400

    Create CartCalculator.java
    cartcalculator

---
+++ src/inheritance/CartCalculator.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class CartCalculator extends Cart{
+
+	public static void main (String [] args) {
+		Calculator cl = new Calculator();
+		
+System.out.println( "Total value: $" + cl.mul(34, 56));
+	
+		
+	}
+}
