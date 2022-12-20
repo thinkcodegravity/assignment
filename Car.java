4879b04329cdb7a3afc4fe844af5a6a179e84211
KeshavJha08 <52606791+KeshavJha08@users.noreply.github.com>
2022-12-20 11:02:16 -0500

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,33 @@
+package example;
+
+public class Car {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car car1 = new Car();
+		car1.start();
+
+	}
+	
+	  // instance variables
+	  private String make;
+	  private String model;
+	  private int year;
+	
+	  // methods
+	  public void start() {
+	    // start the car
+		  System.out.println("Hello");
+	  }
+	
+	  public void drive() {
+	    // drive the car
+	  }
+	
+	  public void stop() {
+	    // stop the car
+	  }
+		
+
+	  
+
+}
