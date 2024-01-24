7fa5ed8f177f549a140f6b8b6670ba5af77706b0
rrsharad8848 <rrsharad8848@gmail.com>
2024-01-24 16:39:20 -0600

    variableAssignment

--- src/Variable/variableAssignment.java
+++ src/Variable/variableAssignment.java
@@ -1,5 +1,50 @@
 package Variable;
 
 public class variableAssignment {
-	int socialSecurityNumber;
+
+    
+    // To store Product Details. Declare and initialize on the same line
+    String productName = "Coffee";
+    int productQuantity = 10;
+    double productPrice = 5.99;
+    
+    // To store last name. Declare and initialize on the same line.
+    String lastName = "Rana";
+    
+    // To Store customer online status. Just Declare
+    Boolean isOnline;
+        
+    // To store value of pi. Just declare
+    float piValue;
+    
+    // To store customer phone number. Declare and initialize on the same line
+    String phoneNumber = "1234567890";
+    
+    // To store customer credit card details. Just declare
+    String creditNum;
+    int cvvNumber;
+    String expDate;
+    
+    // To store height. Declare and initialize on the same line
+    double height = 6.2;
+    
+    // To store employee salary. Declare and initialize on the same line.
+    int salary = 5000;
+    
+    // To store payment details. Declare and initialize on the same line.
+    String creditNumber = "123645568945";
+    int cvvNum = 455;
+    String expireDate = "12/24";
+        
+    // To store bank balance. Just Declare
+    float bankBalance;
+    
+    // To store company name. Declare and initialize on the same line
+    String companyName = "ABC Corp";
+    
+    // To store bank account number. Just Declare
+    double accNumber;
+    
+    // To store shipping address. Declare and initialize on the same line
+    String shippingAddress = "123 Redbull Blvd";
 }
---
+++ src/oopsFundamental/oopsFundamentalassignment.java
@@ -0,0 +1,30 @@
+package oopsFundamental;
+
+public class oopsFundamentalassignment {
+	/*
+	•	Write java class for below example. 
+	•	List following. FYI: 
+	–	Has refers to variable/fields/data of a class, 
+	–	Does refers to methods of the class.
+		1) Product
+			Has/instance variable: name, price, quantity
+			Does/methods: buy, sell, negotiate.
+		2) Car
+			Has/instance variable: brake, streeling wheel, engine, transmission
+			Does/methods: accelerates, brakes, changeDirections.
+		3) Bank
+			Has/instance variable: accountNumber, clientInfo, accountBalance
+			Does/methods: deposits, withdrawal,
+		4) Television 
+			Has/instance variable: PowerSwitch, VolumeButton, Screen
+			Does/methods: changeVolume, displayChannels, 
+		5) Fan
+			Has/instance variable: Blades, Motor, ChangeSpeedSwitch
+			Does/methods: TurnsOn, changeSpeed, Stops.
+		6) Tree
+			Has/instance variable: Leaves, Roots, Branches
+			Does/methods: doesPhotosynthesis, produceFruits, produceOxygen
+
+*/
+
+}
