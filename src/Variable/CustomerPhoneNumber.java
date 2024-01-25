9a7e953fad6712c7b10ed1984588bf7ffad72809
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-24 21:17:31 -0600

    Submitting Customer Online Status

---
+++ src/Variable/CustomerPhoneNumber.java
@@ -0,0 +1,10 @@
+package Variable;
+
+public class CustomerPhoneNumber {
+	// Variable declaration for credit card details
+    String creditCardNumber;
+    String cardHolderName;
+    String expirationDate;
+    int cvv;
+
+}
