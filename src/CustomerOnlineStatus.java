8149daeafb5206aae6528e29d0f467b76958619d
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-25 01:05:32 -0600

    Submitting Credit Card Details

--- src/CustomerOnlineStatus.java
+++
@@ -1,6 +0,0 @@
-
-public class CustomerOnlineStatus {
-    // Variable declaration for Customer Online Status
-    boolean isCustomerOnline;
-
-}
---
+++ src/Variable/CreditCardDetails.java
@@ -0,0 +1,10 @@
+package Variable;
+
+public class CreditCardDetails {
+    // Variable declaration for credit card details
+    String creditCardNumber;
+    String cardHolderName;
+    String expirationDate;
+    int cvv;
+
+}
