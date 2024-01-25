ea90f20d3e6301d2999b2cd2c3ef3e0fd1cc1570
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-24 21:23:03 -0600

    Submitting Variable 1st assignment

---
+++ src/variableAssignment/SocialSecurityNumberDeclaration.java
@@ -0,0 +1,12 @@
+package variableAssignment;
+
+public class SocialSecurityNumberDeclaration {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+		int SocialSecurityNumber;
+		
+	}
+
+}
