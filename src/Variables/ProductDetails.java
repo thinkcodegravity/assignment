88a8bc120ba03dc840ef873ce784a04e2bac0270
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-01-25 12:26:05 -0600

    Submitting product details assignment form variables

---
+++ src/Variables/ProductDetails.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class ProductDetails {
+	//product has a name
+	//Declaring the variable productName 
+	String productName="Gaming PC";
+	
+	//product has a price
+	//Declaring the variable productPrice.
+	double productPrice=1499.99;
+	
+	//product has a color
+	//Declaring the variable productColor 
+	String productColor="Black";
+}
