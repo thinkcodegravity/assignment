88860c5dcda05f6bc901adcbcf32e27caa20bf61
diptrip888 <dipunk888@gmail.com>
2024-01-24 21:42:20 -0600

    Create Has_A_Does_A.java
    Has A & Does A concept from OOPS Fundamentals.

---
+++ src/oopsFundamentals/Has_A_Does_A.java
@@ -0,0 +1,30 @@
+package oopsFundamentals;
+
+public class Has_A_Does_A {
+	
+/*1) Product 
+	Has/instance variable : name, price, quantity
+	Does/methods : buy, sell, negotiate 
+	
+	2) Car
+		Has/instance variable  :Body,Engine,Accelerator,Break,Ignition bottom/Key,Fuel Tank.
+		Does/methods  : Start/Stop, Drives,Controls,Fuel Storage.
+		
+	3) Bank
+		Has/instance variable  : Customer Profile(include customers name, phone number,address, SSN etc), Account Number, Routing Number,Balance.
+		Does/methods  : Log In,Update Profile, Withdrawn,Deposit,Transfer.
+		
+	4) Television 
+		Has/instance variable  : Screen , Body, Remote.
+		Does/methods  :Display, ON/OFF,Channel Change.
+		
+	5) Fan
+		Has/instance variable  : Blades, Button
+		Does/methods  : Rotate, Accelerates/Decelerates,Throw Cold Air.
+		
+	6) Tree
+		Has/instance variable  : Roots,Leaves,Branches,Fruits.
+		Does/methods  : Photosynthesis,Shelter,Shades 
+			
+*/
+}
