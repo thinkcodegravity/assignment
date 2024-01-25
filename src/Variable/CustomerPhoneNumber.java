9b3a9eee820a2fc7b6a49966709f979f2c1e3d7f
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-25 01:02:11 -0600

    Submitting Customer Phone Number

---
+++ src/Variable/CustomerPhoneNumber.java
@@ -0,0 +1,7 @@
+package Variable;
+
+public class CustomerPhoneNumber {
+    // Variable declaration and initialization for Customer Phone Number
+    long customerPhoneNumber = 469878654L;  // Example phone number
+
+}
