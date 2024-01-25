4615f9cacfae38cdc5f9993bfa91925833a194ea
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-25 07:20:40 -0600

    Submitting Company Contact Information

---
+++ src/Variable/CompanyContactInfo.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class CompanyContactInfo {
+    // Variable declaration for Company Contact Information
+    String companyName;
+    String address;
+    String phoneNumber;
+    String email;
+
+}
+
