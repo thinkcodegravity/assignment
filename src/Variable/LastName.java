5961093fbd86d1de6aeae45c82e027d5faf8ff81
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-24 21:14:34 -0600

    Declaring last name

---
+++ src/Variable/LastName.java
@@ -0,0 +1,10 @@
+package Variable;
+
+public class LastName {
+	// Variable declaration for Last Name
+    String lastName;
+    
+    // Variable initialization for Last Name
+    lastName = "Koirala";  // Example last name
+
+}
