cf4e90c7f6ba6a9588849ace92ce405fbe10ce7f
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-25 21:50:54 -0600

    Submitting Product details program

---
+++ src/variableAssignment/productDetails.java
@@ -0,0 +1,16 @@
+package variableAssignment;
+
+public class productDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String productName = "Iphone";
+		short price = 1000;
+		
+	System.out.println("The product is:" + productName );
+	System.out.println("The price of the product is:" + price);
+	
+	}
+
+}
