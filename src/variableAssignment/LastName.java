c4813643dd41606d62aa5ed0b2f4edf2d2eece8b
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-25 21:54:13 -0600

    lastname program

---
+++ src/variableAssignment/LastName.java
@@ -0,0 +1,13 @@
+package variableAssignment;
+
+public class LastName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String LastName;
+		LastName = "Sultana";
+	
+	System.out.println("The last name is :" + LastName);
+	}
+
+}
