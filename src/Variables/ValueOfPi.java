04574097137c8f158d0fd1da23857b8ce2e0adc8
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-01-25 20:54:39 -0600

    Submitting the value of pi assignment from variable chapter

---
+++ src/Variables/ValueOfPi.java
@@ -0,0 +1,7 @@
+package Variables;
+
+public class ValueOfPi {
+//Declaring the variable of pi using double datatype. 
+//double datatype has more precision value than float datatype.
+double pi=3.14159265358979;
+}
