4d1ceeef44b33ee74bb67e1486ab876d0f4ca191
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-25 22:04:39 -0600

    ShippingAddress program

---
+++ src/variableAssignment/ShippingAddress.java
@@ -0,0 +1,17 @@
+package variableAssignment;
+
+public class ShippingAddress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		     String ReceiversName = "Kay";
+		     String streetAddress = "7225 fair Oaks Ave";
+		     String city          = "Dallas";
+		     String state         = "Texas";
+		     String zipCode       = "75229";
+		     
+		  System.out.println(ReceiversName);
+	}
+
+}
