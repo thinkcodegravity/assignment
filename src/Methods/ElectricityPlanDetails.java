2b3f6569944c87bd693f33067811cdf2f4bf02e4
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-27 04:56:32 -0600

    Submitting methods for ElectricityPlanDetails

---
+++ src/Methods/ElectricityPlanDetails.java
@@ -0,0 +1,25 @@
+package Methods;
+
+public class ElectricityPlanDetails {
+
+public String planDetailsFinder(string zipCode)	{
+	
+	if (zipCode=("12345")) {
+		return "Plan:Standard, price 0.15/hr and Renewable is Yes";
+		
+}elseif (zipCode=("67890")) {
+		return "Plan:Premium, price 0.19/hr and Renewable is No";}
+		
+}else {
+	return "No Plan available at this area";
+	}
+				
+	}
+	
+	
+}
+
+
+
+
+
