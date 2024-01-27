6391c69278f8f216308b39d38042592cc0de9bd7
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-26 21:02:41 -0600

    Submitting PaymentDetails

---
+++ src/Variable/PaymentDetails.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class PaymentDetails {
+String CardHolderName;
+String CardNumber;
+String ExpirationDate;
+int cvv; {
+
+CardHolderName = "Mike";
+CardNumber = "345676544";
+ExpirationDate = "08/22";
+cvv = 457;
+}
+}
+
