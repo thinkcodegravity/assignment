3d9818e746c7c2a1f58d69423f6fadf7b0deb9e0
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-27 04:18:07 -0600

    Submitting method that calculate simple interest

---
+++ src/Methods/SimpleInterest.java
@@ -0,0 +1,10 @@
+package Methods;
+
+public class SimpleInterest {
+public double calculateSimpleInterest (double principal, int time, double rate ) {
+	double Interest = ( principal * time * rate) / 100;
+	
+  return Interest ;
+}
+}
+
