9e8cb62e2e4166e33865f0b4e09e287f1b7efe4b
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-01-27 16:11:25 -0600

    Assignment form methods declaration and return statements

---
+++ src/Method/PaypalPayment.java
@@ -0,0 +1,22 @@
+package Method;
+
+public class PaypalPayment {
+//Declaring Variables of PayPal Account
+	String AccountName = "Code Gravity LLC";
+	String email = "abcd@gmail.com";
+
+	// Creating a method PayPalaAyment having double as a return type and double as
+	// a input.
+	public double PayPalPayment(double PaymentAmount) {
+
+		System.out.println(AccountName + " accepted the amount of $" + PaymentAmount + " through PayPal.");
+		return PaymentAmount;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PaypalPayment pay = new PaypalPayment();// Creating reference object pay
+		pay.PayPalPayment(500.15);// method calling statement
+	}
+
+}
