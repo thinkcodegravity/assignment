03444623d35a847fd97bd48422f064dbc170a04f
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-26 21:47:47 -0600

    Creating a method that returns payment response

---
+++ src/Methods/PaymentProcessing.java
@@ -0,0 +1,7 @@
+package Methods;
+
+public class PaymentProcessing {
+public String processPayment(double amount, String CardNumber, String CardHolderName, String ExpirationDate, int cvv) {
+	return"Payment is successful";
+}
+}
