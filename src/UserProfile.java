c51a33dedb96473003b70101ba9c67b2861a936b
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-27 19:01:06 -0600

    Submitting a method that creates user profile

---
+++ src/UserProfile.java
@@ -0,0 +1,23 @@
+
+public class UserProfile {
+public String CustomerProfile(String UserId , String Password) {
+if (UserID.equals("user456") && Password.equals("qwer456")){
+		return ("UserName= John , Email= John34@yahoo.com");
+}else {
+	return ("Invalid UserName or Password. Please Try Again.");
+}
+}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
