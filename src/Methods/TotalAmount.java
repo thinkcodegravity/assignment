37d88c65ccb212e5b0e440370920740864ab9374
Utsav Koirala <utsavkoirala96@gmail.com>
2024-01-27 18:31:50 -0600

    Submitting method for total amount calculation

---
+++ src/Methods/TotalAmount.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class TotalAmount {
+public double AmountCalculation(double ProductPrice, double DiscountPercentage) {
+double TotalAmountCalculation = ProductPrice - (DiscountPercentage * ProductPrice);	
+
+	return TotalAmountCalculation;
+}
+}
+
+
+
+
