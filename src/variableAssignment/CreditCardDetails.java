d2b077ef690fb88fd3e86c1958f0d00fda24c464
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-28 20:54:53 -0600

    creditcard details

---
+++ src/variableAssignment/CreditCardDetails.java
@@ -0,0 +1,14 @@
+package variableAssignment;
+
+public class CreditCardDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String CardHolderName;
+		long CardNumber;
+		String ExpiryDate;
+		short CVV;
+	}
+
+}
