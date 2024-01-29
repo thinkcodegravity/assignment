de2a70d864031b91c83dec81cf252f227ed77fca
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-28 20:45:59 -0600

    online status

---
+++ src/variableAssignment/OnlineStatus.java
@@ -0,0 +1,12 @@
+package variableAssignment;
+
+public class OnlineStatus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Boolean CustomerOnlineStatus;
+		
+	}
+
+}
