f74bd91c1f34f5317e78328faf4d2ba2b629ab13
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-01-29 00:16:01 -0600

    Creating my own calculator program for the assignment use

---
+++ src/Method/Calculator.java
@@ -0,0 +1,33 @@
+package Method;
+public class Calculator {
+	public double sum(double a, double b) {
+		//add method
+		double add=a+b;
+		return add;
+	}
+	
+	
+	//sub method
+public double sub(double a, double b) {
+	double sub=a-b;
+	return sub;
+}
+//Multiplication method
+public double mul(double a, double b) {
+	
+	double mul=a*b;
+	return mul;
+}
+//Division method
+public double div(double a, double b) {
+	
+	double div=a/b;
+	return div;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
