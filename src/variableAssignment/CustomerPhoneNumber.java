312c60c7918bc2fd7734e338e1787bc30b4a22d2
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-28 20:50:16 -0600

    CustomerPhoneNumber

---
+++ src/variableAssignment/CustomerPhoneNumber.java
@@ -0,0 +1,13 @@
+package variableAssignment;
+
+public class CustomerPhoneNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String CustomerPhoneNumber = "459-555-5554";
+	
+	System.out.println("Customer phone number is:" + CustomerPhoneNumber);
+	}
+
+}
