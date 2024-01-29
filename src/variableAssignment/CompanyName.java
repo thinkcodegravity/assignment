18cce2e3613da0ee934136b0daf07975c4c0957a
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-01-28 20:40:56 -0600

    CompanyName

---
+++ src/variableAssignment/CompanyName.java
@@ -0,0 +1,14 @@
+package variableAssignment;
+
+public class CompanyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String CompanyName = "ABC Corporation";
+		
+		System.out.println(CompanyName);
+	}
+	
+
+}
