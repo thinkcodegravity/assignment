f01a5dc1b2bb2976c68ebec51b5ec5b264ecc302
anilboyapati02 <anilboyapati02@gmail.com>
2024-02-02 13:36:24 -0600

    Variable concept _String

---
+++ src/Variable/LastName.java
@@ -0,0 +1,13 @@
+package Variable;
+
+public class LastName {
+	 public static void main(String[] args) {
+	
+	// Declare the variable
+	String lastName;
+
+	// Initialize the variable on the next line
+	lastName = "Boyapati";
+
+}
+}
