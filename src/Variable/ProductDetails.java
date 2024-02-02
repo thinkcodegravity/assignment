83fe387f6d8ad813fe9adb6c3c29df94f525c5b8
anilboyapati02 <anilboyapati02@gmail.com>
2024-02-02 12:21:33 -0600

    Variables concept
    Declare and initialize on same line

---
+++ src/Variable/ProductDetails.java
@@ -0,0 +1,18 @@
+package Variable;
+
+public class ProductDetails {
+	
+	//Let us Say here the product is 'BAG'
+	
+	// Declare and initialize on same line
+	
+	String name= "VIP";
+	double price = 80.99;
+	int weight = 50;
+       int warranty = 5;
+    
+    
+	 
+	
+
+}
