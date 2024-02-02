62de8c2aad847ffd794ae70da5286fa369b4afa8
anilboyapati02 <anilboyapati02@gmail.com>
2024-02-02 15:34:41 -0600

    Assignment

---
+++ src/Variable/PaymentDetails.java
@@ -0,0 +1,20 @@
+package Variable;
+
+public class PaymentDetails {
+
+	public static void main (String[]args) {
+	int AmountPaid;
+	AmountPaid = 676;
+	
+	String PaymetMethod;
+	PaymetMethod = "Card";
+	
+	
+	//TO Store bank balance. just Declare
+	
+	double BankBalance;
+	
+	
+}
+}
+
--- src/Variable/VariableAssignment.java
+++ src/Variable/VariableAssignment.java
@@ -3,8 +3,6 @@ package Variable;
 public class VariableAssignment {
 	
 	int SocialSecurityNumber;
-	String Name;
-	Double number;
 	
 
 }
