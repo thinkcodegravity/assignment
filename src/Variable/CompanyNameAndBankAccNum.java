d74c2166105502412aa63e0f8f66f6d0d9a3e43e
anilboyapati02 <anilboyapati02@gmail.com>
2024-02-02 15:43:37 -0600

    Variable Assignments

---
+++ src/Variable/CompanyNameAndBankAccNum.java
@@ -0,0 +1,13 @@
+package Variable;
+
+public class CompanyNameAndBankAccNum {
+	
+	//To store company name Declare and initialize on same line
+	
+	String CompanyName= "CodeGravity";
+	
+	//To store bank account number Just Declare
+  
+	long BankAccNumber;
+	
+}
---
+++ src/Variable/ShippingAddress.java
@@ -0,0 +1,13 @@
+package Variable;
+
+public class ShippingAddress {
+	
+	// To store shipping address Declare and initialize on same line
+	
+	String CustomerName = "Anil";
+	long MobileNumber = 1234567890;
+	String Address = "4009 N O connor rd";
+	String city = "Irving";
+	int zipcode = 063636;
+	
+}
