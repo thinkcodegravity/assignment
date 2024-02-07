45c555047b24a6ba76382a5d366039e263ed3ddd
Utsav Koirala <utsavkoirala96@gmail.com>
2024-02-07 15:43:50 -0600

    Submitting method to print menu items using switch case

---
+++ src/methodCall/menuItem.java
@@ -0,0 +1,15 @@
+package methodCall;
+
+public class menuItem {
+public static void printMenuItems(int selection) {
+	case 1:
+		System.out.println("1-Pizza");
+		break;
+	case 2:
+		System.out.println("2-Pasta");
+		break;
+		default:
+			System.out.println("Default-Salad");
+			break;
+}
+}
