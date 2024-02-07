5f1d29f0ca80e8ae21634e8f4eb203e6cef518f8
Utsav Koirala <utsavkoirala96@gmail.com>
2024-02-07 15:12:23 -0600

    Submitting a method to print multiple of 5

---
+++ src/methodCall/multiplesOf5.java
@@ -0,0 +1,11 @@
+package methodCall;
+
+public class multiplesOf5 {
+	public static void printMultiplesOf5() {
+	    for (int i = 1; i <= 10; i++) {
+	        int multiple = i * 5;
+	        System.out.println(multiple);
+	    }
+	}
+}
+
