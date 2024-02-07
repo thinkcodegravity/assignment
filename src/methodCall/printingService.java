5b7953c29eb32ef4c1a866e7f80d61fbf55163bc
Utsav Koirala <utsavkoirala96@gmail.com>
2024-02-07 15:34:50 -0600

    Submitting method to print customer care using ternary operator

---
+++ src/methodCall/printingService.java
@@ -0,0 +1,8 @@
+package methodCall;
+
+public class printingService {
+public static void printService(int input) {
+	String output = (input==1)? "customer care" : "other help";
+	System.out.println(output);
+}
+}
