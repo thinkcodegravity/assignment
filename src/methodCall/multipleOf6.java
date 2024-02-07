7dcf822d548d5ccb3bd600ba9bb4a87fef1dd2fa
Utsav Koirala <utsavkoirala96@gmail.com>
2024-02-07 15:20:45 -0600

    Submitting method for multiple of 6 using while loop

---
+++ src/methodCall/multipleOf6.java
@@ -0,0 +1,12 @@
+package methodCall;
+
+public class multipleOf6 {
+	public static void printMultipleOf6(){
+int i=1;
+while(i<=10) {
+	int multiple = i*6;
+	System.out.println(multiple);
+	i++;
+}
+}
+}
