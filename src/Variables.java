2298d64095d6f2a89c2479eab05b2ae24febe2be
pawan lamichhane <pawansir55@gmail.com>
2024-02-07 11:31:40 -0600

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,84 @@
+package assignment.src;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		/*
+		 * All variables should have proper naming convention, java valid name , choose
+		 * appropriate data types. Create variable … create separate java programs and
+		 * include relevant variables below in those programs. to store Social security
+		 * number. Just do the declaration. To store Product Details. Declare and
+		 * initialize on same line To store last name.. Declare and next line
+		 * initialize. To Store customer online status. Just Declare To store value of
+		 * pi . Just declare To store customer phone number. Declare and initialize on
+		 * same line To store customer credit card details. Just declare To store
+		 * company contact info. Just Declare To store height. . Declare and initialize
+		 * on same line To store employee salary. Declare and next line initialize. To
+		 * store add 2 cart details. Just Declare To store payment details. Declare and
+		 * next line initialize. To store bank balance Just Declare
+		 * 
+		 * 
+		 */
+		//to store Social security* number. Just do the declaration.
+		
+		// Declaration of Social Security Number variable
+	    String socialSecurityNumber;
+
+	    // Declaration and initialization of Product Details variable
+	    String productDetails = "Unique Product";
+
+	    // Declaration of Last Name variable
+	    String lastName;{
+	    // Initialization of Last Name variable
+	    lastName = "Lamichhane";
+	    }
+
+	    // Declaration of Customer Online Status variable
+	    boolean isCustomerOnline;
+
+	    // Declaration of Pi variable.
+	    double piValue;
+
+	    // Declaration and initialization of Customer Phone Number variable
+	    long customerPhoneNumber = 1234567890L;
+
+	    // Declaration of Customer Credit Card Details variable
+	    String creditCardDetails;
+
+	    // Declaration of Company Contact Info variable
+	    String companyContactInfo;
+
+	    // Declaration and initialization of Height variable
+	    float heightInMeters = 2.85f;
+
+	    // Declaration of Employee Salary variable
+	    double employeeSalary;
+	    // Initialization of Employee Salary variable
+	    {
+	        employeeSalary = 10000.0;
+	    }
+
+	    // Declaration of Add to Cart Details variable
+	    String addToCartDetails;
+
+	    // Declaration and initialization of Payment Details variable
+	    String paymentDetails = "CASH APP";
+
+	    // Declaration of Bank Balance variable
+	    double bankBalance;
+
+	    // Declaration and initialization of Company Name variable
+	    String companyName = "Core Gravity";
+
+	    // Declaration of Bank Account Number variable
+	    String bankAccountNumber;
+
+	    // Declaration and initialization of Shipping Address variable
+	    String shippingAddress = "123 Lake trl, Fort Worth, TX";
+		
+	}
+
+	}
+
+
