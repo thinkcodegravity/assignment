f2f1dffb61e5b113c6de3ac923574e921e4f6938
gautammerina3 <41892351+gautammerina3@users.noreply.github.com>
2024-02-07 09:20:18 -0600

    Variables

---
+++ Variables.java
@@ -0,0 +1,44 @@
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		String socialSecurityNumber;
+		
+		String productName = "Phone";
+		double productPrice = 29.99;
+		int productQuantity = 100;
+		
+		String lastName;
+		lastName = "Gautam";
+		
+		boolean isOnline;
+		
+		double pi = 3.14159;
+		
+		String phoneNumber = "123-456-7890";
+		
+		String creditCardDetails;
+		
+		String companyContactInfo;
+		
+		double height = 160.5;
+		
+		double employeeSalary;
+		employeeSalary = 50000.00;
+		
+		String addToCartDetails;
+		
+		String paymentDetails = "Credit Card";
+		
+		double bankBalance;
+	
+		String companyName = "Code Gravity";
+		
+		String bankAccountNumber;
+		
+		String shippingAddress = "Dallas, Texas";
+	}
+	
+	
+
+}
