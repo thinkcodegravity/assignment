c65428bf372983a3fddf867438dc277fc256f74d
Utsav Koirala <utsavkoirala96@gmail.com>
2024-02-07 01:42:58 -0600

    Submitting method to calculate simple interest

---
+++ src/DataStructure/BankingSystem.java
@@ -0,0 +1,5 @@
+package DataStructure;
+
+public class BankingSystem {
+
+}
---
+++ src/methodCall/calculateInterest.java
@@ -0,0 +1,8 @@
+package methodCall;
+
+public class calculateInterest {
+ double calculationOfInterset(double Principal , double rate , double time){
+double interest = (Principal*rate*time)/100;
+return interest;
+}
+}
