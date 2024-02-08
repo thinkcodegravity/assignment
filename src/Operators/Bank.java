6c091881701df1af6f075855fc9544b55137ea5d
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-02-08 02:06:56 -0500

    Bank program from operators

---
+++ src/Operators/Bank.java
@@ -0,0 +1,29 @@
+package Operators;
+
+public class Bank {
+//Declaring band initializing variables of bank
+	double balance = 2500.00;
+
+//Creating a method for deposit
+	public double Deposit(int depositAmount) {
+		balance += depositAmount;
+		System.out.println("Balance After Deposit: $" + balance);
+		return balance;
+	}
+
+	// Creating a method for withdraw
+	public double Withdraw(int withdrawAmount) {
+		balance -= withdrawAmount;
+		System.out.println("Balance After Withdraw: $" + balance);
+		return balance;
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank bal = new Bank();
+		bal.Deposit(500);
+		bal.Withdraw(1000);
+	}
+
+}
