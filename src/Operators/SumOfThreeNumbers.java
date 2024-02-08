c2de441e27c7b5bbb59ae35951413ce8ff8dba1e
Sandesh244 <sandeshkhatiwada24@gmail.com>
2024-02-08 02:17:00 -0500

    Create SumOfThreeNumbers.java

---
+++ src/Operators/SumOfThreeNumbers.java
@@ -0,0 +1,19 @@
+package Operators;
+
+public class SumOfThreeNumbers {
+	// Creating a method AddNumbers to get sum of three numbers
+
+	public int AddNumbers(int a, int b, int c) {
+		int sum = a + b + c;
+		System.out.println("Sum: "+sum);
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SumOfThreeNumbers s = new SumOfThreeNumbers();
+		s.AddNumbers(5, 10, 15);
+	}
+
+}
