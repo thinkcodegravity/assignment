cfb16d52c51a2b46d1f61d147f7340e82b95bd63
Sadikshya52 <158636448+Sadikshya52@users.noreply.github.com>
2024-02-10 21:44:22 -0500

    Assignment Reference

---
+++ src/Reference/TestFb.java
@@ -0,0 +1,16 @@
+package Reference;
+
+public class TestFb {
+	public static void main(String[] args) { 
+		int x;
+		int y= 20;
+		Fb fb1= new Fb();
+		Fb fb2= new Fb();
+		
+		System.out.println(fb1.name);
+		
+		System.out.println(fb2.age);
+		
+	}
+		
+}
