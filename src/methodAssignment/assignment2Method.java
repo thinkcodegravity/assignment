123e3a447a19d764b5d346dd80e5b0cd5122ba39
Freya991 <157636534+Freya991@users.noreply.github.com>
2024-02-11 20:37:56 -0600

    Method_processRegistrtion

---
+++ src/methodAssignment/assignment2Method.java
@@ -0,0 +1,16 @@
+package methodAssignment;
+
+public class assignment2Method {
+	
+	public static String processRegistration(String UserDetails) {
+	
+		return (UserDetails + "Success");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
