267d6204d9231c3a2083a02f7376ca8cfbee5fb8
Sandesh Oli <sundaesholi@gmail.com>
2024-02-16 01:02:00 -0600

    variableScope

--- src/variableScope/Age.java
+++ src/variableScope/Age.java
@@ -1,10 +1,19 @@
 package variableScope;
 
 public class Age {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	String ageGroup = "30-39";
+	int BMR = 1296;
+	
+	public void metabolism (int age) {
+		System.out.println("Your metabolsim is on track with your age based on calorie consumption");
+		
+		return;
 	}
 
+
+
+
 }
+
+
