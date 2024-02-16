1581068f8c9ef01fb4dfacaf9754b7808d0ade6d
Saini Yanala <33129168+saini07@users.noreply.github.com>
2024-02-16 09:45:27 -0600

    Initial Commit

---
+++ src/OOPS/OopsConcept.java
@@ -0,0 +1,28 @@
+package OOPS;
+
+public class OopsConcept {
+	/*1) Product
+	Has/instance variable : name, price, quantity
+	Does/methods : buy, sell, negotiate
+	
+	2) Car
+	Has/instance variable  :Steering, Accelerator, Brakes, Engine, Fuel tank
+	Does/methods  :driving, start, change gear, stop, reverse
+	
+	3) Bank
+	Has/instance variable  :account number, customer id, phone, email, routing number
+	Does/methods  : withdraw, deposit, transfer
+	
+	4) Television 
+	Has/instance variable  :model,monitor, remote, color, brand
+	Does/methods  :change channel, turn on/off, increase/decrease volume
+	
+	5) Fan
+	Has/instance variable  : model, brand
+	Does/methods  : air cooling system, filter air
+	
+	6) Tree
+	Has/instance variable  :fruits,flower
+	Does/methods  :produce oxygen,
+	*/
+}
