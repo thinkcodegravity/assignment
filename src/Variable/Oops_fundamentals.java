2a2e2d5edbfa30816ebb9145bcf419f70343a407
Somnath0408 <159497847+Somnath0408@users.noreply.github.com>
2024-02-23 12:41:26 -0600

    Update Oops_fundamentals.java

--- src/Variable/Oops_fundamentals.java
+++ src/Variable/Oops_fundamentals.java
@@ -1,5 +1,21 @@
 package Variable;
 
 public class Oops_fundamentals {
+car ;
+	has-a : steering,wheels,lights,gear box.
+	does-a: driving,turning.
+	bank;
+	has-a :bank_account,credit_card,debit_card,customer,employee,
+	does-a: deposit,withdraw, 
+	televison;
+	has-a : screen, remote.
+	does-a : watch.
+	fan;
+	has-a : motor, wings,
+	does-a : blow_air.
+	tree;
+	has-a: leaves, branches, fruits,roots.
+	does-a : gives shade
+
 
 }
