e75773320eb3594aab61135c3ecc6ad73220ad70
Nikita Poudel <nikitapoudel10@gmail.com>
2024-03-19 18:51:57 -0500

    VariableAssignment

--- src/Variable/VariableAssignment.java
+++ src/Variable/VariableAssignment.java
@@ -1,7 +1,102 @@
 package Variable;
 
+	
+
 public class VariableAssignment {
 
-	String socialSecurityNumber;
-	
+		String socialSecurityNumber;
+		
 }
+	//To store Product Details. Declare and initialize on same line
+public class productDetail {
+	 String name = “IPhone”;
+	 double price = 799.99;
+	 int quantity= 5;
+}
+
+//		To store last name.. Declare and next line initialize.
+public class variableNaming {
+	 String last_Name;
+	 Last_Name = “pant”;
+}
+
+//	,	To Store customer online status. Just Declare
+public class variableNaming {
+	 boolean onlineStatus;
+}
+
+//		To store value of pi . Just declare
+public class variableNaming {
+	 double valueOFPi;
+}
+
+//		To store customer phone number. Declare and initialize on same line
+public class variableNaming {
+	 int phoneNumber = 1111111111;
+}
+
+	// To store customer credit card details. Just declare
+public class variableNaming {
+	 int card_Number;
+	 int cvv;
+	 int expiry_Date;
+}
+
+//		To store company contact info. Just Declare
+public class variableNaming {
+	 String email;
+	 int phone_Number;
+	 int fax_Number;
+}
+
+//		To store height. . Declare and initialize on same line
+public class variableNaming {
+	 double height = 5.9;
+}
+
+//		To store employee salary. Declare and next line initialize.
+public class variableNaming {
+	 double salary;
+	 Salary = 1099.99;
+}
+
+//		To store add 2 cart details. Just Declare
+public class variableNaming {
+	 String first_Item;
+	 String second_item;
+	 String cart;
+	 cart = first_Item + second_item;
+}
+
+//		To store payment details. Declare and next line initialize.
+public class variableNaming {
+	 int card_Number;
+	 Card_Number = 222233334444;
+	 int expiry_Date;
+	 Expiry_Date = 826;
+	 int cvv;
+	 cvv =000;
+}
+
+//		To store bank balance Just Declare
+public class variableNaming {
+
+	 double balance;
+}
+//		To store company name Declare and initialize on same line
+public class variableNaming {
+	 String company_Name = “Apple”;
+}
+
+//		To store bank account number Just Declare
+public class variableNaming {
+	 int account_Number;
+}
+
+//		To store shipping address Declare and initialize on same line
+public class variableNaming {
+	 Addr address = “ 1111 Jenna Way, Rosenberg, TX, 78999”;
+}
+
+
+
