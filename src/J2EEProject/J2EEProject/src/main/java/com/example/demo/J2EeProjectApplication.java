c0e4677e4383d023ddb5f41279d92c0cdee02cd8
srikanthg97 <srikanthreddyg97@gmail.com>
2024-03-20 15:32:40 -0400

    CG-srikanth goli-profilej2ee
    CG-srikanth goli-profilej2ee

---
+++ src/J2EEProject/J2EEProject/src/main/java/com/example/demo/J2EeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2EeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(J2EeProjectApplication.class, args);
+	}
+
+}
---
+++ src/J2EEProject/J2EEProject/src/test/java/com/example/demo/J2EeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2EeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
